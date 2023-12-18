bb85ca4a11523cb1dbcf783a83d67312869999f5
JeffersonSolis <1374414@senati.pe>
2023-12-18 11:01:53 -0500

    sincronizando

--- crm/webapp/forms.py
+++ crm/webapp/forms.py
@@ -8,14 +8,9 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
 
-#Register/Create a user
 
-class CreateUserForm(UserCreationForm):
 
-    class Meta:
 
-        model = User
-        fields = ['username','password1','password2']
 
 
 #Login a user
